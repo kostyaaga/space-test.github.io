0bf672ab09d988b1aaa7ecc6cd7d1352283a8679
kostyaaga <kostyafan2006@gmail.com>
2025-11-20 22:17:37 +0700

    gh pages fix bug routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import './styles/app.scss';
-import {Provider} from "react-redux";
-import {store} from "./redux/store.ts";
-
+import "./styles/app.scss";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 const rootElem = document.getElementById("root");
 
 if (rootElem) {
-    const root = ReactDOM.createRoot(rootElem);
-    root.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>,
-    );
+  const root = ReactDOM.createRoot(rootElem);
+  root.render(
+    <BrowserRouter basename="/space-test">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>,
+  );
 }
